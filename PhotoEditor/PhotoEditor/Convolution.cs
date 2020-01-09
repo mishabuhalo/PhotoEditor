a072d3dead6f33f4dc58b011f1fcef1b1d832291
Misha Bukhalo <bukhalo1999@gmai.com>
2020-01-09 15:35:52 +0200

    Added Convolution class for processing convolution filters

---
+++ PhotoEditor/PhotoEditor/Convolution.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace PhotoEditor
+{
+   public static  class Convolution
+    {
+        public static Bitmap ConvolutionMethod<T>(this Bitmap input, T filter) where T : ConvolutionFilters
+        {
+            BitmapData sourceData = input.LockBits(new Rectangle(0, 0, input.Width, input.Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+
+
+            byte[] inputBuffer = new byte[sourceData.Stride * sourceData.Height];
+            byte[] resultBuffer = new byte[sourceData.Stride * sourceData.Height];
+
+            Marshal.Copy(sourceData.Scan0, inputBuffer, 0, inputBuffer.Length);
+
+            input.UnlockBits(sourceData);
+
+            double Red = 0.0;
+            double Green = 0.0;
+            double Blue = 0.0;
+
+            int filterWidth = filter.filterMatrix.GetLength(0);
+            int filterHeight = filter.filterMatrix.GetLength(1);
+
+            int filterOffset = (filterWidth - 1) / 2;
+
+            int neighbourPixel = 0;
+
+            int pixel = 0;
+
+            for (int i = filterOffset; i < input.Height - filterOffset; i++)
+            {
+                for (int j = filterOffset; j < input.Width - filterOffset; j++)
+                {
+                    Red = 0;
+                    Green = 0;
+                    Blue = 0;
+
+                    pixel = i * sourceData.Stride * j * 4;
+
+                    for (int k = -filterOffset; k <= filterOffset; k++)
+                    {
+                        for (int l = -filterOffset; l <= filterOffset; l++)
+                        {
+                            neighbourPixel = pixel + (j * 4) + (i * sourceData.Stride);
+
+                            Red += (inputBuffer[neighbourPixel + 2]) * filter.filterMatrix[i + filterOffset, j + filterOffset];
+                            Green += (inputBuffer[neighbourPixel + 1]) * filter.filterMatrix[i + filterOffset, j + filterOffset];
+                            Blue += (inputBuffer[neighbourPixel]) * filter.filterMatrix[i + filterOffset, j + filterOffset];
+                        }
+                    }
+
+                    Blue = filter.Factor * Blue + filter.Bias;
+                    Green = filter.Factor * Green + filter.Bias;
+                    Red = filter.Factor * Red + filter.Bias;
+
+                    if (Blue > 255)
+                        Blue = 255;
+                    else if (Blue < 0)
+                        Blue = 0;
+
+                    if (Green > 255)
+                        Green = 255;
+                    else if (Green < 0)
+                        Green = 0;
+
+                    if (Red > 255)
+                        Red = 255;
+                    else if (Red < 0)
+                        Red = 0;
+
+                    resultBuffer[pixel] = (byte)Blue;
+                    resultBuffer[pixel + 1] = (byte)Green;
+                    resultBuffer[pixel + 2] = (byte)Red;
+                    resultBuffer[pixel + 3] = 255;
+
+                }
+            }
+            Bitmap resultBitmap = new Bitmap(input.Width, input.Height);
+
+            BitmapData resultData = resultBitmap.LockBits(new Rectangle(0, 0, resultBitmap.Width, resultBitmap.Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+
+            Marshal.Copy(resultBuffer, 0, resultData.Scan0, resultBuffer.Length);
+
+            resultBitmap.UnlockBits(resultData);
+
+            return resultBitmap;
+
+        }
+    }
+}
